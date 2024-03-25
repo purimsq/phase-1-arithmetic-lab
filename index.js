fda07d7cee05d531d0615873f919e755fb140e2c
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-03-25 12:03:00 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2
+let random = Math.floor(Math.random() * 10);
+let num3 = 8;
+let num4 = 2;
+let mod = num3/num4
+let max = Math.max(20,4,12,1,3);
